58b51291ead013fd9f9e709d6c0e1f933f962cac
ccrawford4 <calum.crawford@joinhandshake.com>
2025-11-23 18:59:56 -0800

    html support

--- init.lua
+++ init.lua
@@ -305,6 +305,21 @@ require('lazy').setup({
     end,
   },
 
+  -- Auto-close and auto-rename HTML/XML tags
+  {
+    'windwp/nvim-ts-autotag',
+    event = 'InsertEnter',
+    config = function()
+      require('nvim-ts-autotag').setup {
+        opts = {
+          enable_close = true, -- Auto close tags
+          enable_rename = true, -- Auto rename pairs of tags
+          enable_close_on_slash = true, -- Auto close on trailing </
+        },
+      }
+    end,
+  },
+
   -- Mark visualization in sign column
   {
     'chentoast/marks.nvim',
